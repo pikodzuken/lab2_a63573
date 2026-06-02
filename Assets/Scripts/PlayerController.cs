7c77f1968c8f2db03946c5125e8fbb84b24f15cc
Pi <ines.f.ribeiro12@gmail.com>
2026-06-02 12:22:01 +0100

    lab 4 todos os passos

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    private float speed = 10.0f;
+    private float speed = 100.0f;
     private float zBound = 6;
     private Rigidbody playerRb;
 
@@ -40,6 +40,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if (transform.position.z > zBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with an enemy.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
         }
     }
 }
